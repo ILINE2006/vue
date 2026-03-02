3a7192152f9bd4303cb2f8e5abbeaf347e970834
ILINE2006 <teplotom71@gmail.com>
2026-03-02 21:09:31 +0700

    Практическая работа 5:
    - Добавлена кнопка "Remove from cart" в index.html
    - Создан метод removeFromCart() в main.js
    - Добавлена проверка, чтобы cart не уходил в минус

--- js/main.js
+++ js/main.js
@@ -1,11 +1,35 @@
 let app = new Vue({
-  el: '#app',
-  data: {
-    product: "Socks",
-    image: "./assets/vmSocks-green-onWhite.jpg",
-    altText: "A pair of socks",
-    inStock: true,
-    details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-    sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
-  }
+    el: '#app',
+    data: {
+        product: "Socks",
+        image: "./assets/vmSocks-green-onWhite.jpg",
+        altText: "A pair of socks",
+        inStock: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ],
+        cart: 0
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+            console.log("Добавили товар, сейчас в корзине: " + this.cart)
+        },
+        removeFromCart() {
+            if (this.cart > 0) {
+                this.cart -= 1
+                console.log("Убрали товар, сейчас в корзине: " + this.cart)
+            } else {
+                console.log("Корзина уже пустая!")
+            }
+        }
+    }
 })
