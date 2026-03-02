8858c8b14c66ecf320103d31bd2006bf505ee218
ILINE2006 <teplotom71@gmail.com>
2026-03-02 16:17:13 +0700

    Практическая 4: - добавил массив sizes в объект data - создал список <ul> с элементами <li> - использовал директиву v-for для перебора массива

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 let app = new Vue({
   el: '#app',
   data: {
-      product: "Socks",
-      image: "./assets/vmSocks-green-onWhite.jpg",
-      altText: "A pair of socks",
-      inStock: true,
-      onSale: true
+    product: "Socks",
+    image: "./assets/vmSocks-green-onWhite.jpg",
+    altText: "A pair of socks",
+    inStock: true,
+    details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+    sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
   }
 })
