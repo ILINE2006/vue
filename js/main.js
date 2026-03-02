51d24a38d23ee8e43ee4ddd5bcc1cf77a18fc82c
ILINE2006 <teplotom71@gmail.com>
2026-03-02 22:02:58 +0700

    Практическая работа 6: добавлено зачёркивание Out of Stock

--- js/main.js
+++ js/main.js
@@ -1,35 +1,31 @@
 let app = new Vue({
     el: '#app',
     data: {
-        product: "Socks",
-        image: "./assets/vmSocks-green-onWhite.jpg",
-        altText: "A pair of socks",
-        inStock: true,
-        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-        variants: [
-            {
-                variantId: 2234,
-                variantColor: 'green'
-            },
-            {
-                variantId: 2235,
-                variantColor: 'blue'
-            }
-        ],
-        cart: 0
+      product: "Socks",
+      image: "./assets/vmSocks-green-onWhite.jpg",
+      altText: "A pair of socks",
+      inStock: false,
+      details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+      variants: [
+        {
+          variantId: 2234,
+          variantColor: 'green',
+          variantImage: "./assets/vmSocks-green-onWhite.jpg",
+        },
+        {
+          variantId: 2235,
+          variantColor: 'blue',
+          variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+        }
+      ],
+      cart: 0
     },
     methods: {
-        addToCart() {
-            this.cart += 1
-            console.log("Добавили товар, сейчас в корзине: " + this.cart)
-        },
-        removeFromCart() {
-            if (this.cart > 0) {
-                this.cart -= 1
-                console.log("Убрали товар, сейчас в корзине: " + this.cart)
-            } else {
-                console.log("Корзина уже пустая!")
-            }
-        }
+      addToCart() {
+        this.cart += 1
+      },
+      updateProduct(variantImage) {
+        this.image = variantImage
+      }
     }
-})
+  })
